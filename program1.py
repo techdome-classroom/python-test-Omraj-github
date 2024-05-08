b985dff24f5991003f039ce95d46fb3ac7f7cdd8
Omraj-github <omrajdhore01@gmail.com>
2024-05-08 10:46:37 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,8 +4,8 @@ class Solution:
     #    write your code here
         def islandsExp(row, col, visited)
             directions = [(-1,0),(0,1), (1, 0),(0, -1)]
-
+            visited[row][col] = True
             for dr, dc in directions:
-                
+
 
         return 0
