9c54d1635fa13456ada8043c8a4d8ce79550aa06
Omraj-github <omrajdhore01@gmail.com>
2024-05-08 11:06:22 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -16,16 +16,16 @@ def decode_message( s: str, p: str) -> bool:
                 # Iterate through the secret message until finding the next character that matches the one after '*' in the decoder key
                 while s_index < len(s) and s[s_index] != p[p_index]:
                         s_index += 1
-                elif p[p_index] == '?':
-                # If the current symbol in the decoder key is '?', it can match any single letter
-                # Move to the next character in both the secret message and the decoder key
-                s_index += 1
-                p_index += 1
-                elif s[s_index] == p[p_index]:
-                # If the characters match, move to the next character in both the secret message and the decoder key
-                s_index += 1
-                p_index += 1
-                else:
+                        elif p[p_index] == '?':
+                        # If the current symbol in the decoder key is '?', it can match any single letter
+                        # Move to the next character in both the secret message and the decoder key
+                        s_index += 1
+                        p_index += 1
+                        elif s[s_index] == p[p_index]:
+                        # If the characters match, move to the next character in both the secret message and the decoder key
+                        s_index += 1
+                        p_index += 1
+                        else:
                 # If none of the above conditions are met, the decoder key doesn't match the message
             return False
     
