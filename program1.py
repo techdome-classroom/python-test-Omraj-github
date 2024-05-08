1bf7cd1ea63add9ec4696764f3259ef94abfded2
Omraj-github <omrajdhore01@gmail.com>
2024-05-08 10:50:02 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,11 +2,13 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-        def islandsExp(row, col, visited)
+        def islandsExp(row, col, visited):
             directions = [(-1,0),(0,1), (1, 0),(0, -1)]
             visited[row][col] = True
+
             for dr, dc in directions:
+                nrow = 
                 islandsExp(row+dr, col+dc, visited)
-                
+
 
         return 0
