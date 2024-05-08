08704bfcb5d78966da04f4e6625e84938a385bb0
Omraj-github <omrajdhore01@gmail.com>
2024-05-08 11:14:37 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,24 +1,24 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
 
-        s_index = 0 
-        p_index = 0  
+    s_index = 0
+    p_index = 0
 
-        while s_index < len(s) and p_index < len(p):
-                if p[p_index] == '*':
-                 p_index += 1
-                
-                if p_index == len(p):
-                        return True
-                
-                        while s_index < len(s) and s[s_index] != p[p_index]:
-                                s_index += 1
-                elif p[p_index] == '?':
-                        s_index += 1
-                        p_index += 1
-                elif s[s_index] == p[p_index]:
-                        s_index += 1
-                        p_index += 1
-                else:
-                        return False
-    
-        return s_index == len(s) and p_index == len(p)
+    while s_index < len(s) and p_index < len(p):
+        if p[p_index] == '*':
+            p_index += 1
+
+        if p_index == len(p):
+            return True
+
+            while s_index < len(s) and s[s_index] != p[p_index]:
+                s_index += 1
+        elif p[p_index] == '?':
+            s_index += 1
+            p_index += 1
+        elif s[s_index] == p[p_index]:
+            s_index += 1
+            p_index += 1
+        else:
+            return False
+
+    return s_index == len(s) and p_index == len(p)
