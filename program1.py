81d64cda0c4851a865b64ee33629c3b48ece0bdd
Omraj-github <omrajdhore01@gmail.com>
2024-05-08 10:45:41 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,7 +2,8 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
+        def islandsExp(row, col, visited)
         directions = [(-1,0),(0,1), (1, 0),(0, -1)]
 
-        
+
         return 0
